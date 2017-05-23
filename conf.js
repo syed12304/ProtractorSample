5e66846ab055cee88404f5858b6bebf23669402a
syed12304 <syed12304@gmail.com>
2017-05-23 20:17:21 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,7 +1,6 @@
 var jasmineReporters = require('jasmine-reporters');
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://0.0.0.0:4444/wd/hub',
   jasmineNodeOpts:{
 		  grep : ''
   },
@@ -14,62 +13,4 @@ exports.config = {
     },
 
   specs:['testSpec.js'],
-
-  /* onPrepare: function() {
-	  var fs = require('fs-extra');
-
-		fs.emptyDirSync('reports/', function (err) {
-        if(err!=null)
-				  console.log("error in cleaning the reports folder : "+err);
-			});
-			
-			fs.ensureDir('reports/screenshots/',function(err){
-        if(err!=null)
-				  console.log("error in creating the screenshots folder in reports : "+err);
-			});
-
-			jasmine.getEnv().addReporter({
-				specDone: function(result) {
-					if (result.status == 'failed') {
-						browser.getCapabilities().then(function (caps) {
-							var browserName = caps.get('browserName');
-
-							browser.takeScreenshot().then(function (png) {
-								var stream = fs.createWriteStream('reports/screenshots/' + browserName + '-' + result.fullName+ '.png');
-								stream.write(new Buffer(png, 'base64'));
-								stream.end();
-							});
-						});
-					}
-				}
-			});
-
-      jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-			consolidateAll: true,
-			savePath: 'reports/',
-			filePrefix: 'xmlresults'
-		}));
-   },
-  onComplete: function() {
-     var browserName, browserVersion;
-     var capsPromise = browser.getCapabilities();
-
-     capsPromise.then(function (caps) {
-        browserName = caps.get('browserName');
-        browserVersion = caps.get('version');
-
-        var HTMLReport = require('protractor-html-reporter');
-
-        testConfig = {
-            reportTitle: 'Test Execution Report',
-            outputPath: './reports',
-            screenshotPath: 'screenshots',
-            testBrowser: browserName,
-            browserVersion: browserVersion,
-            modifiedSuiteName: false,
-            screenshotsOnlyOnFailure: true
-        };
-        new HTMLReport().from('./reports/xmlresults.xml', testConfig);
-    });
- }*/
 }
